4fcdc5f06b5ada8d7305689947bfdcccab62ebd4
astral4 <88992929+astral4@users.noreply.github.com>
2025-02-28 03:39:51 -0800

    resize image

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use anyhow::{Result, bail};
+use fast_image_resize::{PixelType, ResizeOptions, Resizer, images::Image};
 use image::ImageReader;
 use pico_args::Arguments;
 use std::{fmt::Display, path::PathBuf, str::FromStr};
 use walkdir::WalkDir;
+
+const PALETTE_IMAGE_WIDTH: u32 = 16;
+const PALETTE_IMAGE_HEIGHT: u32 = 16;
 
 fn parse_arg<T>(args: &mut Arguments, (short, long): (&'static str, &'static str)) -> Result<T>
 where
@@ -42,6 +46,15 @@ fn main() -> Result<()> {
         }
 
         let image = ImageReader::open(path)?.decode()?.into_rgba8();
+        let (input_width, input_height) = image.dimensions();
+
+        let image =
+            Image::from_vec_u8(input_width, input_height, image.into_vec(), PixelType::U8x4)?;
+
+        let mut resized_image =
+            Image::new(PALETTE_IMAGE_WIDTH, PALETTE_IMAGE_HEIGHT, PixelType::U8x4);
+
+        Resizer::new().resize(&image, &mut resized_image, &ResizeOptions::default())?;
     }
 
     Ok(())
